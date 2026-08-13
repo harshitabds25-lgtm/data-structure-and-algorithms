8784e49024e0865b1b34ff7c01315534ae436468
harshitabds25-lgtm <harshitab.ds25@sbjit.edu.in>
2026-08-13 23:50:32 +0530

    Time: 14 ms (93.38%) | Memory: 17.3 MB (99.26%) - LeetSync

---
+++ 18-4sum/4sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> res;
+        int n = nums.size();
+        if (n < 4) return res;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // skip duplicates
+
+                long long newTarget = (long long)target - nums[i] - nums[j];
+                int left = j + 1, right = n - 1;
+
+                while (left < right) {
+                    long long sum = nums[left] + nums[right];
+                    if (sum == newTarget) {
+                        res.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        left++;
+                        right--;
+                        while (left < right && nums[left] == nums[left - 1]) left++;
+                        while (left < right && nums[right] == nums[right + 1]) right--;
+                    } else if (sum < newTarget) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
