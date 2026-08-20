7064550c1248d593f65f9f8771c6aee99b9298e3
harshitabds25-lgtm <harshitab.ds25@sbjit.edu.in>
2026-08-20 22:33:56 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (66.37%) - LeetSync

---
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> arr1, arr2;
+        
+        // First two operations
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+        
+        // Remaining operations
+        for (int i = 2; i < nums.size(); i++) {
+            if (arr1.back() > arr2.back()) {
+                arr1.push_back(nums[i]);
+            } else {
+                arr2.push_back(nums[i]);
+            }
+        }
+        
+        // Concatenate arr1 and arr2
+        arr1.insert(arr1.end(), arr2.begin(), arr2.end());
+        return arr1;
+    }
+};
