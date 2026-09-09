ed5e856307a00e679e599ab7450313816c7fada9
harshitabds25-lgtm <harshitab.ds25@sbjit.edu.in>
2026-09-09 17:35:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (84.80%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0;
+
+        for (long long p = 1000; p <= n; p *= 1000) {
+            ans += (n - p + 1);
+
+            if (p > LLONG_MAX / 1000) break;
+        }
+
+        return ans;
+    }
+};
